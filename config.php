ed64cd8151e128ec7b94ffcc167693ba7f4eb63f
MarryisHotter <141461126+MarryisHotter@users.noreply.github.com>
2024-11-28 00:10:16 +0100

    Fixed being unable to create channels and stuff

--- config.php
+++ config.php
@@ -4,11 +4,4 @@ $servername = "mysql";
 $dbUsername = "mario";
 $dbPassword = "zTUMGzNToCinAzEcdIlGWIOAu";
 $dbname = "chat_app";
-
-$conn = new mysqli($servername, $dbUsername, $dbPassword, $dbname);
-
-if ($conn->connect_error) {
-    error_log('Database connection error: ' . $conn->connect_error);
-    die('Database connection error.');
-}
 ?>
